53803accba581ef4ea08d620e2dd02df77f984a2
yogvid <yogvidwankhede@gmail.com>
2025-09-05 00:42:34 -0500

    docs: Update project dependencies

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(
+    name="HealthMate-AI",
+    version="0.1.0",
+    author="Yogvid Vishwas Wankhede",
+    author_email="yogvidwankhede@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
